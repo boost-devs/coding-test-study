88998e241097c02f390c8a1376403bbda5cab7f4
changwoomon <kamme10@naver.com>
2021-05-11 00:29:19 +0900

    fix2

--- adc/graph_traversal/2606_바이러스.py
+++ adc/graph_traversal/2606_바이러스.py
@@ -9,7 +9,7 @@ input = sys.stdin.readline
 def dfs(v, visited):
     # 현재 노드를 방문 처리
     visited[v] = True
-#     print(v, end=' ')
+    # print(v, end=' ')
     # 현재 노드와 연결된 다른 노드를 재귀적으로 방문
     for i in graph[v]:
         if not visited[i]:
