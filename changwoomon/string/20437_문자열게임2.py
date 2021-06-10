cbf2bd2d268a6c80df99731f1b7d21f5a2b35759
changwoomon <kamme10@naver.com>
2021-06-10 20:31:00 +0900

    solve 20437

---
+++ changwoomon/string/20437_문자열게임2.py
@@ -0,0 +1,36 @@
+###### 20437번: 문자열 게임 2
+# https://www.acmicpc.net/problem/20437
+# 메모리/시간: 32156KB / 280ms
+
+import sys
+from collections import deque, Counter, defaultdict
+
+input = sys.stdin.readline
+
+T = int(input())
+
+def str_game(dict):
+    global K
+    length = deque([-1])
+    for k, v in dict.items():
+        if len(v) >= K:
+            for i in range(len(v)-K+1):
+                length.append(v[i+K-1] - v[i] + 1)
+    return length
+
+for i in range(T):
+    W = input().rstrip()
+    K = int(input().rstrip())
+    c = Counter(W)
+    if max(c.values()) < K:
+        print(-1)
+        continue
+    d = defaultdict(list)
+    for i, x in enumerate(W):
+        d[x].append(i)
+    game = str_game(d)
+    if len(game) == 1:
+        print(-1)
+    else:
+        game.popleft()
+        print(f"{min(game)} {max(game)}")
