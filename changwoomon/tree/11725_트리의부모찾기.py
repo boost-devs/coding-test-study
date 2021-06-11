03911f09dd0f0de0e7807d285fd37883cfe17963
changwoomon <kamme10@naver.com>
2021-06-11 16:24:28 +0900

    change

--- changwoomon/tree/11725_트리의부모찾기.py
+++ changwoomon/tree/11725_트리의부모찾기.py
@@ -1,8 +1,9 @@
 # 11725번: 트리의 부모 찾기
 # https://www.acmicpc.net/problem/11725
-# 메모리/시간: 132912KB / 408ms
+# 메모리/시간: 49220KB / 420ms
 
 import sys
+from collections import deque
 
 sys.setrecursionlimit(10**9)
 input = sys.stdin.readline
@@ -10,20 +11,23 @@ input = sys.stdin.readline
 N = int(input())
 
 tree = [[] for _ in range(N+1)]
-answer = [0 for _ in range(N+1)]
+parent = [0 for _ in range(N+1)]
 
 for _ in range(N-1):
-    s, e = map(int, input().split())
-    tree[s].append(e)
-    tree[e].append(s)
+    start, end = map(int, input().split())
+    tree[start].append(end)
+    tree[end].append(start)
 
-def dfs(s, parent):
-    for i in tree[s]:
-        if parent[i] == 0:
-            parent[i] = s
-            dfs(i, parent)
+def bfs(start, parent):
+    queue = deque([start])
+    while queue:
+        node = queue.popleft()
+        for i in tree[node]:
+            if parent[i] == 0:
+                queue.append(i)
+                parent[i] = node
 
-dfs(1, answer)
+bfs(1, parent)
 
 for i in range(2, N+1):
-    print(answer[i])
+    print(parent[i])
