0d53e5812d3e58c06486d0a3e740cffdee30fa7c
changwoomon <kamme10@naver.com>
2021-06-11 15:58:03 +0900

    solve 9934

---
+++ changwoomon/tree/9934_완전이진트리.py
@@ -0,0 +1,25 @@
+###### 9934번: 완전 이진 트리
+# https://www.acmicpc.net/problem/9934
+# 메모리/시간: 29200KB / 68ms
+
+import sys
+
+input = sys.stdin.readline
+
+K = int(input())
+
+node = list(map(int, input().split()))
+node.insert(0, 0)
+answer = [[] for _ in range(K)]
+
+for i in range(K-1, -1, -1):
+    j = 2**i
+    tmp = []
+    while j < len(node):
+        if node[j] != 0:
+            answer[i].append(node[j])
+            node[j] = 0
+        j += 2**i
+
+for i in range(K-1, -1, -1):
+    print(" ".join(map(str, answer[i])))
