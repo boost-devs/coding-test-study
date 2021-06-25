94ac8abb158e73150d91ec9f9df8f3528ee70b8c
안유진 <an-yujin@an-yujin-ui-MacBookPro.local>
2021-06-26 00:31:55 +0900

    [감자] 2021.06.25

--- dkswndms4782/dynamic_programming_1/2579_계단오르기.cpp
+++ dkswndms4782/dynamic_programming_1/2579_계단오르기.cpp
@@ -2,7 +2,6 @@
 #include <algorithm>
 using namespace std;
 
-int stairs[301] = { 0, };
 //현재 한칸이면 [n][1], 현재 두칸째이면 [n][2]
 int score[301][3] = { (0,0,0), };
 
@@ -11,12 +10,12 @@ int main() {
 	cin.tie(NULL);
 	int N; cin >> N;
 	for (int i = 1; i <= N; i++)
-		cin >> stairs[i];
-	score[1][1] = stairs[1];
-	score[2][1] = stairs[2];  score[2][2] = score[1][1] + stairs[2];
+		cin >> score[i][0];
+	score[1][1] = score[1][0];
+	score[2][1] = score[2][0];  score[2][2] = score[1][1] + score[2][0];
 	for (int i = 3; i <= N; i++) {
-		score[i][2] = score[i - 1][1] + stairs[i];
-		score[i][1] = max(score[i - 2][1], score[i - 2][2]) + stairs[i];
+		score[i][2] = score[i - 1][1] + score[i][0];
+		score[i][1] = max(score[i - 2][1], score[i - 2][2]) + score[i][0];
 	}
 	cout << max(score[N][1], score[N][2]);
 }
