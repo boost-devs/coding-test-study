992d57ff432bfcc13e9b6b347c369cc4cccb0a06
changwoomon <kamme10@naver.com>
2021-06-29 00:15:59 +0900

    solve

---
+++ changwoomon/dynamic_programming_1/1912_연속합.py
@@ -0,0 +1,22 @@
+###### 1912번: 연속합
+# https://www.acmicpc.net/problem/1912
+# 메모리/시간: 37288KB / 124ms
+
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+
+A = tuple(map(int, input().split()))
+
+table = [-9999 for _ in range(n)]
+
+for i in range(n):
+    table[i] = A[i]
+    if i == 0:
+        continue
+    if table[i] < table[i-1] + A[i]:
+        table[i] = table[i-1] + A[i]
+
+print(max(table))
