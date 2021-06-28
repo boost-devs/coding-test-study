4e29280ea31dad1e1ebc93e267d928c1d65ad98b
안유진 <an-yujin@an-yujin-ui-MacBookPro.local>
2021-06-29 03:29:50 +0900

    a

--- dkswndms4782/dynamic_programming_1/11727_2xn타일링2.cpp
+++ dkswndms4782/dynamic_programming_1/11727_2xn타일링2.cpp
@@ -3,10 +3,10 @@ using namespace std;
 
 int arr[1001];
 int dp(int n) {
-    if(n == 1) return 1;
-    if(n == 2) return 3;
+	if (n == 1) return 1;
+	if (n == 2) return 3;
 	if (arr[n] > 0) return arr[n];
-	arr[n] = dp(n - 1) + dp(n - 2) * 2;
+	arr[n] = dp(n - 1) + dp(n - 2)*2;
 	arr[n] %= 10007;
 	return arr[n];
 }
