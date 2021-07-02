12ae273b3ba8e23f0c962e39dd89284df88fe9f0
opijae <0206cjh@daum.net>
2021-07-03 02:54:15 +0900

    20210702

--- opijae/dynamic_programming_1/1106_호텔.py
+++ opijae/dynamic_programming_1/1106_호텔.py
@@ -2,7 +2,7 @@ import sys
 input = sys.stdin.readline
 target_num , n = map(int, input().split())
 
-dp=[0]*1000000 # 최소 비용의 dp. dp[1]은 비용 1일때 최대 손님 수
+dp=[0]*1000000 # 최소 비용의 dp. dp[1]은 비용 1일때 최대 손님 수    
 arr=[]
 for _ in range(n):
     arr.append(list(map(int, input().split())))
