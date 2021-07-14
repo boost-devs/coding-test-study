27dad33416c67e25b5bfd266b2bb712aee6bf3bb
CoodingPenguin <coodingpenguin@gmail.com>
2021-07-14 23:35:52 +0900

    Add comments

--- coodingpenguin/implementation/14467_소가길을건너간이유1.py
+++ coodingpenguin/implementation/14467_소가길을건너간이유1.py
@@ -7,15 +7,17 @@ from collections import defaultdict
 
 input = sys.stdin.readline
 
-n = int(input())
-count = 0
-loc = defaultdict(int)
+n = int(input())  # 관찰 횟수
+count = 0  # 길을 건너간 최소 횟수
+loc = defaultdict(int)  # 소의 최근 위치
 for _ in range(n):
-    a, b = map(int, input().split())
+    a, b = map(int, input().split())  # 번호, 위치
+    # 최근 위치 정보가 없다면
     if a not in loc:
         loc[a] = b
+    # 최근 위치 정보가 있다면
     else:
         if loc[a] != b:
-            loc[a] = b
+            loc[a] = b  # 갱신
             count += 1
 print(count)
