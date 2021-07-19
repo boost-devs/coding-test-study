2bcefee0c2d2c229a38027faaf01be1064188b8d
changwoomon <kamme10@naver.com>
2021-07-20 04:39:31 +0900

    change

--- changwoomon/implementation/10994_별찍기-19.py
+++ changwoomon/implementation/10994_별찍기-19.py
@@ -9,13 +9,13 @@ input = sys.stdin.readline
 N = int(input())
 
 for i in range(1, N):
-    j = (N-i)*4 + 1
+    j = (N-i) * 4 + 1
     print("* " * (i-1) + "*" * j + " *" * (i-1))
     print("* " * i + " " * (j-4) + " *" * i)
 
 print("* " * (N*2-1))
 
 for i in range(N-1, 0, -1):
-    j = (N-i)*4 + 1
+    j = (N-i) * 4 + 1
     print("* " * i + " " * (j-4) + " *" * i)
     print("* " * (i-1) + "*" * j + " *" * (i-1))
