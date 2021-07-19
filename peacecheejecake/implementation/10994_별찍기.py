55d70544b99eacdb4136eb9cd1eb9d566da56716
Jiwon <peace.cheejecake@gmail.com>
2021-07-19 23:58:26 +0900

    Fix 10994.

--- peacecheejecake/implementation/10994_별찍기.py
+++ peacecheejecake/implementation/10994_별찍기.py
@@ -1,5 +1,5 @@
 # https://www.acmicpc.net/problem/10994
-# 별 찍기
+# 별 찍기 - 19
 # 29592 KB / 124 ms
 
 
@@ -12,7 +12,7 @@ for i in range(l):
     for j in range(l):
         if (
             i % 2 == 0 and abs(m - j) <= abs(m - i) or
-            j % 2 == 0  and abs(m - i) <= abs(m - j)
+            j % 2 == 0 and abs(m - i) <= abs(m - j)
         ):
             sky[i][j] = '*'
 
