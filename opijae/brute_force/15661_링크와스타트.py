a00a81e0b26ea7ef21cc7f0ede453b27690e3e95
opijae <0206cjh@daum.net>
2021-08-17 01:32:05 +0900

    20210816-1

--- opijae/brute_force/15661_링크와스타트.py
+++ opijae/brute_force/15661_링크와스타트.py
@@ -9,18 +9,18 @@ arr = []
 for _ in range(n):
     arr.append(list(map(int, input().split())))
 
-cases = product([0,1],repeat=n)
+cases = list(product([0,1],repeat=n))
 _min = 1000000
-
-for case in cases:
+for case in cases[:(2**n)+1]:  # 절반까지만 보면됨
     team1 = []
     team2 = []
-    for i in range(n):
+    for i in range(n): # 비트마스크에 따른 팀 분배
         if case[i]:
             team2.append(i)
         else:
             team1.append(i)
     _team1 = 0
+    # 팀 점수 구하기
     for member in team1:
         for synergy in team1:
             if member != synergy:
