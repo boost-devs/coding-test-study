9d193a909890b8a8ca8f5bf4bb9bf7ce36bf4a6c
keeprainy <abyss7500@snu.ac.kr>
2021-10-03 21:47:46 +0900

    Add Sep week4 leetcode PS

---
+++ keeprainy/leetcode/1143_Longest_Common_Subsequence.py
@@ -0,0 +1,26 @@
+"""
+#  LeetCode
+#  ver.Python3
+#
+#  Created by GGlifer
+#
+#  Open Source
+"""
+
+from typing import *
+
+
+class Solution:
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        dp = [[0 for _ in range(len(text2)+1)] for _ in range(len(text1)+1)]
+        for i in range(1, len(text1)+1):
+            for j in range(1, len(text2)+1):
+                if text1[i-1] == text2[j-1]:
+                    dp[i][j] = dp[i-1][j-1] + 1
+                else:
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+        return dp[len(text1)][len(text2)]
+
+
+if __name__ == '__main__':
+    solution = Solution()
---
+++ keeprainy/templateLC.py
@@ -0,0 +1,19 @@
+"""
+#  LeetCode
+#  ver.Python3
+#
+#  Created by GGlifer
+#
+#  Open Source
+"""
+
+from typing import *
+
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        pass
+
+
+if __name__ == '__main__':
+    solution = Solution()
