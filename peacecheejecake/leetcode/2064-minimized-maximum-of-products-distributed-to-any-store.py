67ac377c60627ae22e439c33919077e62ffc7018
Jiwon <peace.cheejecake@gmail.com>
2021-11-09 22:53:51 +0900

    improve binary search

--- peacecheejecake/leetcode/2064-minimized-maximum-of-products-distributed-to-any-store.py
+++ peacecheejecake/leetcode/2064-minimized-maximum-of-products-distributed-to-any-store.py
@@ -3,23 +3,16 @@ from math import ceil
 
 class Solution:
     def minimizedMaximum(self, n: int, quantities: List[int]) -> int:
-        
-        def _req_stores(x):
-            return sum([ceil(q / x) for q in quantities])
-        
         min_x, max_x = 1, max(quantities)
         while min_x < max_x - 1:
             x = (min_x + max_x) // 2
-            req_stores = _req_stores(x)
+            req_stores = sum([ceil(q / x) for q in quantities])
             if req_stores > n:
-                min_x = x
+                min_x = x + 1
             elif req_stores <= n:
                 max_x = x
-        
-        if _req_stores(min_x) > n:
-            return max_x
         return min_x
 
 
-# Runtime: 1772 ms
-# Memory Usage: 28.6 MB
+# Runtime: 1864 ms
+# Memory Usage: 28.3 MB
