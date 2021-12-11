9f6a4a60d3cd00f14e1fbba32a884abfd2cd1485
Jiwon <peace.cheejecake@gmail.com>
2021-12-12 00:08:05 +0900

    solve 211211

---
+++ peacecheejecake/programmers/43166_타겟넘버.py
@@ -0,0 +1,9 @@
+def solution(numbers, target):
+    l = len(numbers)
+    answer = 0
+    for i in range(1 << l):
+        signs = f"{i:0{l}b}" # 0이면 +, 1이면 -
+        case = sum((-1) ** int(t) * num for t, num in zip(signs, numbers))
+        answer += int(case == target)
+    return answer
+    
